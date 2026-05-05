66175e8007af41d9ee99544114240034cac30d08
nlafevers <nathan.lafevers@gmail.com>
2026-05-04 23:49:47 -0400

    Support HEAD requests via chi GetHead middleware

--- cmd/kopds/main.go
+++ cmd/kopds/main.go
@@ -73,6 +73,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
+	r.Use(middleware.GetHead)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
